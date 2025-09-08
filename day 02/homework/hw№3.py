366087896e99e749044b961a16324c6c2d420bce
GiorgiGhvdze <ghvinadzegiorgi0@gmail.com>
2025-09-08 15:34:39 +0400

    third commit

---
+++ day 02/homework/hw№3.py
@@ -0,0 +1,13 @@
+name = "Giorgi"
+surname = "Ghvinadze"
+city = "Tbilisi"
+
+height = 1.69
+weight = 56.7
+age = 13.92
+
+number =  145
+grade = 8 
+year = 2011
+
+print("my name is", name, surname, "i'm", age, "years old.", "I was born in", year, "and live in", city, ". i'm", height, "cm tall and i weight", weight, "kilograms.", "im an", grade, "th grader student and i study in №", number, "public school.")
