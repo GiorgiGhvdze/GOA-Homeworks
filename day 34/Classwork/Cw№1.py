17602e767076f0240520822df311fe692b036b8e
GiorgiGhvdze <ghvinadzegiorgi0@gmail.com>
2025-12-07 14:40:44 +0400

    35th commit

---
+++ day 34/Classwork/Cw№1.py
@@ -0,0 +1,9 @@
+students = ["Giorgi", "Vaja", "Abraham", "Nikolozi", "Vaja"]
+
+students.append("Lali")
+students.pop(1)
+students.count("Vaja")
+students.index("Abraham")
+students.reverse()
+len(students)
+students.clear()
---
+++ day 34/Classwork/Cw№2.py
@@ -0,0 +1,9 @@
+fruits = {"apple", "orange", "banana"}
+
+vegetables = {"apple", "cucumber"}
+
+fruits.add("pear")
+fruits.difference(vegetables)
+fruits.pop("apple")
+fruits.union(vegetables)
+fruits.clear()
